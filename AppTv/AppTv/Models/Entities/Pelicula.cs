fc69f5755c4ea63febad6a48ef265bedeeb9044e
serna <serna0667@gmail.com>
2024-02-27 20:05:35 +0100

    add folders

---
+++ AppTv/AppTv/Models/Entities/Pelicula.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AppTv.Models.Entities
+{
+    class Pelicula
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Category { get; set; }
+        public bool Subtitles { get; set; }
+        public string Language { get; set; }
+        public string Description { get; set; }
+    }
+}
